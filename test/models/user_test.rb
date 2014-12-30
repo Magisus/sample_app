ce418a284c37e863584ff2222449ce2a4823fc62
Maggie <darthmagisus@gmail.com>
2014-12-30 22:22:49 +0000

    Finished login/logout

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -59,4 +59,8 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
   
+  test "authenticated? should return false if remember_digest is nil" do
+    assert_not @user.authenticated?('')
+  end
+  
 end
